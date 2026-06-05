abd983f90aa6fa6c48b78a5e7aaab5a79a9834b2
auraecosystem <sandraisom@icloud.com>
2026-06-05 01:16:54 +0100

    Update and rename bash.sh to bash.fish

--- bash.fish
+++ bash.fish
@@ -5,3 +5,13 @@ npx -y firebase-tools@latest deploy --only dataconnect
 npx -y firebase-tools@latest \
          dataconnect:execute dataconnect/seed_data.gql
          npx -y firebase-tools@latest dataconnect:sdk:generate
+
+npx -y firebase-tools@latest \
+          apps:create web react-example
+npx -y firebase-tools@latest \
+          apps:sdkconfig web \
+          -o web-app/src/firebase-config.json
+cd web-app
+npm i firebase \
+            @tanstack/react-query \
+            @tanstack-query-firebase/react

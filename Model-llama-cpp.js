1fbb19b9e451c34aa6f5ce38c7c20d55f018e1fc
auraecosystem <sandraisom@icloud.com>
2026-06-05 01:47:39 +0100

    Create Model-llama-cpp.js

---
+++ Model-llama-cpp.js
@@ -0,0 +1,23 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import {getLlama, LlamaChatSession} from "node-llama-cpp";
+
+const __dirname = path.dirname(
+    fileURLToPath(import.meta.url)
+);
+
+const llama = await getLlama();
+const model = await llama.loadModel({
+    modelPath: path.join(__dirname, "my-model.gguf")
+});
+const context = await model.createContext();
+const session = new LlamaChatSession({
+    contextSequence: context.getSequence()
+});
+
+
+const q1 = "Hi there, how are you?";
+console.log("User: " + q1);
+
+const a1 = await session.prompt(q1);
+console.log("AI: " + a1);

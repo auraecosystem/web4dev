974def55feda5ac638a0fb37eeb02cdd4b5a0c86
auraecosystem <sandraisom@icloud.com>
2026-06-05 01:15:58 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,7 @@
 mkdir myproj ; cd myproj
 npx -y firebase-tools@latest login --reauth
 npx -y firebase-tools@latest init dataconnect
+npx -y firebase-tools@latest deploy --only dataconnect
+npx -y firebase-tools@latest \
+         dataconnect:execute dataconnect/seed_data.gql
+         npx -y firebase-tools@latest dataconnect:sdk:generate
